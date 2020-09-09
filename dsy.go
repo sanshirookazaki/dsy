a4ecf7d09e8242c5492b5ac035259a5be4d3e661
sanshirookazaki <sanshirookazaki@gmail.com>
2020-09-10 01:02:35 +0900

    implement batch

--- dsy.go
+++ dsy.go
@@ -3,10 +3,15 @@ package dsy
 import (
 	"context"
 	"io/ioutil"
+	"math"
 	"path/filepath"
 	"strings"
 
 	"cloud.google.com/go/datastore"
+)
+
+const (
+	BatchSize = 500
 )
 
 func UpsertFile(ctx context.Context, client *datastore.Client, filename string) (err error) {
@@ -21,9 +26,19 @@ func UpsertFile(ctx context.Context, client *datastore.Client, filename string) 
 		return err
 	}
 
-	keys, values := getKeysValues(entities)
-	if _, err = client.PutMulti(ctx, keys, values); err != nil {
-		return err
+	allPage := int(math.Ceil(float64(len(entities)) / float64(batchSize)))
+	for page := 0; page < allPage; page++ {
+
+		from := page * BatchSize
+		to := (page + 1) * BatchSize
+		if to > len(entities) {
+			to = len(entities)
+		}
+
+		keys, values := getKeysValues(entities, from, to)
+		if _, err = client.PutMulti(ctx, keys, values); err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -88,8 +103,8 @@ func dirwalk(dir string) []string {
 	return paths
 }
 
-func getKeysValues(entities []*datastore.Entity) (keys []*datastore.Key, values []interface{}) {
-	for _, e := range entities {
+func getKeysValues(entities []*datastore.Entity, from, to int) (keys []*datastore.Key, values []interface{}) {
+	for _, e := range entities[from:to] {
 		keys = append(keys, e.Key)
 		props := datastore.PropertyList(e.Properties)
 		values = append(values, &props)
